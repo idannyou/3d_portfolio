609be576c6b739c052a7c99c861b487b1208aac8
Danny Ou <idannyou@gmail.com>
2023-02-13 16:56:53 -0800

    Add animation

--- main.js
+++ main.js
@@ -13,14 +13,41 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+// which DOM to render
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
-}); // which DOM to render
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight); //make it full screen
-camera.position.setZ(30); //initally in the middle, move it to 30
+//make it full screen
+renderer.setSize(window.innerWidth, window.innerHeight);
+//initally in the middle, move it to 30
+camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+//what material to wrap your shape
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus); //need to add scene
+
+function animate() {
+  // The window.requestAnimationFrame() method tells the browser
+  // that you wish to perform an animation and requests
+  // that the browser calls a specified function
+  // to update an animation before the next repaint.
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+// game loop aka while loop to keep rendering via animate function
+animate();
