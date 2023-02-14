b24a4b85b64eb817692b1ca2dfea06b1dd0aa189
Danny Ou <idannyou@gmail.com>
2023-02-13 17:07:02 -0800

    Add orbit control

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -28,13 +29,32 @@ renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 //what material to wrap your shape
-const material = new THREE.MeshBasicMaterial({
+// MeshStandardMaterial reflects light
+// MeshBasicMaterial is just a mesh
+const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
-  wireframe: true,
 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus); //need to add scene
+
+// pointlight creates a light bulb at a point
+// const pointLight = new THREE.PointLight(0xffffff);
+// pointLight.position.set(20, 20, 20);
+// scene.add(pointLight);
+
+// // helper that shows where the light is
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// scene.add(lightHelper);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+
+// ambientlight is like a flood light
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight, gridHelper);
+
+// OrbitControls allows interaction 3D
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   // The window.requestAnimationFrame() method tells the browser
@@ -45,6 +65,8 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
